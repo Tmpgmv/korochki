d7df8ff2fb8a19e9ea5c61124e117885a93a8d80
Граблевский Михаил Владимирович <Grablevskiy@pkgh.edu>
2026-05-07 18:37:36 +0300

    019

--- models/User.php
+++ models/User.php
@@ -35,9 +35,9 @@ class User extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['admin'], 'default', 'value' => ''0''],
+            [['admin'], 'default', 'value' => 0],
             [['username', 'password', 'full_name', 'phone', 'email'], 'required'],
-            [['admin'], 'string'],
+            [['admin'], 'integer'],
             [['username', 'password', 'full_name', 'phone', 'email'], 'string', 'max' => 255],
             [['username'], 'unique'],
         ];
