3a16b78b22e459c91359e0453d397129771fb37a
Граблевский Михаил Владимирович <Grablevskiy@pkgh.edu>
2026-05-07 15:15:18 +0300

    015

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,7 +32,7 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 <header id="header">
     <?php
     NavBar::begin([
-        'brandLabel' => Html::img('@web/images/logo.png', ['alt' => 'My logo',  'class'=> 'me-1', 'width' => 65]) . Yii::$app->name,
+        'brandLabel' => Html::img('@web/images/logo.png', ['alt' => 'My logo',  'class'=> 'me-1', 'width' => 65]) . "<span class='logo'>" . Yii::$app->name . "</span>",
         'brandUrl' => Yii::$app->homeUrl,
         'options' => ['class' => 'navbar-expand-md navbar-dark bg-primary fixed-top']
     ]);
