9f6a14086254f0e8a6d6c7732c317662d857c800
Граблевский Михаил Владимирович <Grablevskiy@pkgh.edu>
2026-05-07 13:38:55 +0300

    003

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=user_korochki',
+    'username' => 'user_korochki',
+    'password' => 'Password1',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
