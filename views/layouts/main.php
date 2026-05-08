aa4adef4fc6af7ad5e67e2c0deb35ffc32b599af
Your Name <you@example.com>
2026-05-08 14:32:40 +0300

    033

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -74,9 +74,6 @@ $this->registerLinkTag(['rel' => 'icon', 'type' => 'image/x-icon', 'href' => Yii
 
 <main id="main" class="flex-shrink-0" role="main">
     <div class="container">
-        <?php if (!empty($this->params['breadcrumbs'])): ?>
-            <?= Breadcrumbs::widget(['links' => $this->params['breadcrumbs']]) ?>
-        <?php endif ?>
         <?= Alert::widget() ?>
         <?= $content ?>
     </div>
