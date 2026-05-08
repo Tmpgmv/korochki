4274f34e2476888dbc589805c2f92e5833f77852
Your Name <you@example.com>
2026-05-08 15:03:39 +0300

    035

--- views/app/_form.php
+++ views/app/_form.php
@@ -25,7 +25,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'feedback')->textarea(['rows' => 6]) ?>
 
     <div class="form-group">
-        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::submitButton('Отправить', ['class' => 'btn btn-success']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
--- views/app/create.php
+++ views/app/create.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\App $model */
 
-$this->title = 'Create App';
+$this->title = 'Новая заявка';
 $this->params['breadcrumbs'][] = ['label' => 'Apps', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
