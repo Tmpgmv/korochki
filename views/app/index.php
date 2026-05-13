366a643e9b02c89f976a4091b46e5c554ba60e82
Your Name <you@example.com>
2026-05-13 08:33:12 +0300

    057

--- views/app/index.php
+++ views/app/index.php
@@ -43,6 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
             
             [
                 'class' => ActionColumn::className(),
+                'template' => '{view}',
                 'urlCreator' => function ($action, App $model, $key, $index, $column) {
                     return Url::toRoute([$action, 'id' => $model->id]);
                  }
