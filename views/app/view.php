078561ded1de2922a6014c9000e6cdbfae2db34b
Your Name <you@example.com>
2026-05-13 08:06:05 +0300

    055

--- views/app/view.php
+++ views/app/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /** @var yii\web\View $this */
 /** @var app\models\App $model */
 
-$this->title = $model->id;
+$this->title = "Заявка " . $model->id;
 $this->params['breadcrumbs'][] = ['label' => 'Apps', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
